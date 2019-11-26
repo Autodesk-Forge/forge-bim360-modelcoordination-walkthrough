646c497269bf2f11338123c20312fed302649d09
Don <don.whittle@autodesk.com>
2019-11-26 15:08:37 +0000

    get manifest and updated ps1 doc

--- src/Automation/Samples/query-modelset-index.ps1
+++ src/Automation/Samples/query-modelset-index.ps1
@@ -1,5 +1,6 @@
-﻿# set the environment up
-#. ([System.IO.Path]::Combine($PSScriptRoot, 'build-and-import-module.ps1'));
+﻿# make sure you run build-and-import-module.ps1 before attempting to run this sample. The following
+# cmdlets also assume that you have persisted a token in the Sampleconfiguraiton.json file. You can 
+# use the AuthWeb ASP.NET Blazor web application to sign into Forge and obtain a token.
 
 $container = 'f83cef12-deef-4771-9feb-4f85643e3c46';
 $modelset = 'b412a75f-bcfa-4566-a8ef-edb974709817';
@@ -21,8 +22,8 @@ $allFields = Get-ModelSetIndexFields -Container $container `
                                      -Version $verison;
 
 # run a query
-#$query = "select * from s3object s where s.p6637df3c = 'Concrete - Cast-in-Place Concrete - Footing'";
-$query = "select sum(s.p2707f0a3 * s.p2b0fd73f * s.p15f8f5ec) + sum(3.14 * ((s.p8fcbb8f7 / 2)*(s.p8fcbb8f7 / 2)) * s.pca1638c2) as volume from s3object s"
+$query = "select * from s3object s where s.p6637df3c = 'Concrete - Cast-in-Place Concrete - Footing'";
+#$query = "select sum(s.p2707f0a3 * s.p2b0fd73f * s.p15f8f5ec) + sum(3.14 * ((s.p8fcbb8f7 / 2)*(s.p8fcbb8f7 / 2)) * s.pca1638c2) as volume from s3object s"
 
 "Run $query";
 
@@ -38,6 +39,3 @@ $rows = Get-IndexRows -Path $resultPath;
 $objects = $rows | %{ $_.Data.ToString() } | ConvertFrom-Json;
 
 $objects;
-
-
-#$rows | %{ [string]($_.Data[$fields[0].Key]) };
